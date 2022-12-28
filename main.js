84752fcb074d9fde6d1051ef5e898f66df2810ed
Georgiana Crainic <cg.crainicgeorgiana@gmail.com>
2022-12-28 21:05:34 +0200

    am adaugat un log in js

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
     const nameInput = document.querySelector('#name');
     const todoList = document.querySelector('#todo-list');
+
+    console.log("Init name is " + nameInput);
 })
 
